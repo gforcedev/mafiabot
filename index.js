dfe7c57c9677b43d8fe68647bf0e4b56afad19f8
gforcedev <tecshotclark@gmail.com>
2019-08-27 17:45:38 +0100

    Find mentioned users with regexes

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require("./config.json");
+const config = require('./config.json');
 
-client.on('ready', () => {
-});
+const mentionRegexp = /<@!?(\d+)>/;
+const mentionRegexpGlobal = /<@!?\d+>/g;
+client.on('ready', () => {});
 
-client.on('message', async (message) => {
+client.on('message', async message => {
 	if (!message.content.startsWith(config.prefix) || message.author.bot || message.channel instanceof Discord.DMChannel) return;
 
 	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
 	const command = args.shift().toLowerCase();
 
 	if (command === 'mafia') {
-		let mentionedUsers = Array.from(message.mentions.members.filter(u => !u.bot).keys());
+		let mentionedUsers = [];
+		for (const mentionText of message.content.match(mentionRegexpGlobal)) {
+			console.log(mentionText);
+			mentionedUsers.push(client.users.get(mentionText.match(mentionRegexp)[1])); // The id (using the group)
+		}
+		console.log(mentionedUsers);
+
 		const chosenMafia = mentionedUsers[Math.floor(Math.random() * mentionedUsers.length)];
-		for (let userid of mentionedUsers) {
-			let u = client.users.get(userid);
-			if (userid === chosenMafia) {
-				u.send(`${u.username}, you are the MAFIA for this round (game created by ${message.author.username})`).catch(e => {
-				});
+		for (let user of mentionedUsers) {
+			console.log(`sending message to ${user.username} (id ${user.id})`);
+			if (user.id === chosenMafia.id) {
+				user.send(`${user.username}, you are the MAFIA for this round (game created by ${message.author.username})`).catch(() => {});
 				break;
 			} else {
-				u.send(`${u.username}, you are a VILLAGER for this round (game created by ${message.author.username})`).catch(e => {
-				});
+				user.send(`${user.username}, you are a VILLAGER for this round (game created by ${message.author.username})`).catch(() => {});
 			}
 		}
 	}
