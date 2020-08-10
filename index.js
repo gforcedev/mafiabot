83ff580ccc1697c6a22012382ad6a111499d23b3
Gforcedev <tomjackclark@gmail.com>
2020-08-10 21:34:56 +0100

    Add a game keyword

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const randomWord = require('random-word');
 const client = new Discord.Client();
 const config = require('./config.json');
 
@@ -15,6 +16,7 @@ client.on('message', async message => {
 	const command = args.shift().toLowerCase();
 
 	if (command === 'mafia') {
+		const gameKeyword = randomWord();
 		let mentionedUsers = [];
 		for (const mentionText of message.content.match(mentionRegexpGlobal)) {
 			mentionedUsers.push(client.users.get(mentionText.match(mentionRegexp)[1])); // The id (using the group)
@@ -23,11 +25,11 @@ client.on('message', async message => {
 		const chosenMafia = mentionedUsers[Math.floor(Math.random() * mentionedUsers.length)];
 		for (let user of mentionedUsers) {
 			if (user.id === chosenMafia.id) {
-				user.send(`${user.username}, you are the MAFIA for this round (game created by ${message.author.username})`).catch(() => {message.channel.send('Ah shoot. I probably got ratelimited or something')});
+				user.send(`${user.username}, you are the MAFIA for this round (game keyword: ${gameKeyword})`).catch(() => {message.channel.send('Ah shoot. I probably got ratelimited or something')});
 				break;
 			}
 		}
-		message.channel.send('Mafia assigned :eyes:');
+		message.channel.send(`Mafia assigned :eyes: - game keyword: ${gameKeyword}`);
 	}
 });
 
